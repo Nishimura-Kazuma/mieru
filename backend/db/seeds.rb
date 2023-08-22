455b65e036506fe34beedd8cd4db694ddee046c8
Nishimura-Kazuma <nishimura.k1999@gmail.com>
2023-08-22 12:31:38 +0900

    データベースの完成

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -8,3 +8,12 @@
 Sample.create(title: "sample1", description: "This is sample data.", checked: true)
 Sample.create(title: "sample2", description: "This is also sample data.", checked: false)
 Sample.create(title: "sample3", description: "Again, this is sample data.", checked: false)
+
+users = User.create!(
+  [
+    {name: '田中',attribute: '保護者' },
+    {name: '鈴木',attribute: '保護者' },
+    {name: '木村',attribute: '保育士' },
+    {name: '橘',attribute: '保育士' },
+  ]
+)
