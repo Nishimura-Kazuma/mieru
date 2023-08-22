484b2176896f57547f2acff8bc48ad5c31c88972
Suzumura Yuki <s2110357@gl.cc.uec.ac.jp>
2023-08-22 12:26:20 +0900

    route-sample

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,41 +1,54 @@
 import { useEffect, useState } from 'react';
 import { getSamples } from './api';
-// import logo from "./logo.svg";
-import './App.css';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+// import PostDetailPage from './pages/PostDetailPage';
+// import EditPostPage from './pages/EditPostPage';
+// import AccoutChange from './pages/AccountChange';
 
-type Sample = {
-  id: number;
-  title: string;
-  description: string;
-  completed: boolean;
-};
+// type Sample = {
+//   id: number;
+//   title: string;
+//   description: string;
+//   completed: boolean;
+// };
 
 const App = () => {
   // Todoリストの初期値を空の配列に設定
-  const [todos, setTodos] = useState<Sample[]>([]);
+  // const [todos, setTodos] = useState<Sample[]>([]);
 
-  useEffect(() => {
-    const fetchTodos = async () => {
-      try {
-        const todosData = await getSamples();
-        setTodos(todosData);
-      } catch (error) {
-        console.error('Error while fetching todos:', error);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchTodos = async () => {
+  //     try {
+  //       const todosData = await getSamples();
+  //       setTodos(todosData);
+  //     } catch (error) {
+  //       console.error('Error while fetching todos:', error);
+  //     }
+  //   };
 
-    fetchTodos();
-  }, []);
+  //   fetchTodos();
+  // }, []);
 
   return (
-    <div className="container">
-      <h1>ToDo List</h1>
-      <ul>
-        {todos.map((sample: any) => (
-          <li key={sample.id}>{sample.title}</li>
-        ))}
-      </ul>
-    </div>
+    // <div className="container">
+    //   <h1>ToDo List</h1>
+    //   <ul>
+    //     {todos.map((sample: any) => (
+    //       <li key={sample.id}>{sample.title}</li>
+    //     ))}
+    //   </ul>
+    // </div>
+
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/post" component={HomePage} />
+        {/* <Route path="/comment" component={PostDetailPage} />
+        <Route path="/comment" component={EditPostPage} />
+        <Route path="/account" component={AccountChange} />
+        <Route component={NotFound} /> */}
+      </Switch>
+    </BrowserRouter>
   );
 };
 
--- frontend/app/src/pages/HomePage.tsx
+++ frontend/app/src/pages/HomePage.tsx
@@ -1,1 +1,5 @@
-export {};
+function HomePage() {
+  return <h1>Home</h1>;
+}
+
+export default HomePage;
