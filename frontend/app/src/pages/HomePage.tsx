0dffc2ed967128ff9981cd7e3555f243547b314f
syoma <kudosyoma@gmail.com>
2023-08-23 16:29:46 +0900

    フロントの機能とデザインの統合

--- frontend/app/src/pages/HomePage.tsx
+++ frontend/app/src/pages/HomePage.tsx
@@ -1,20 +1,39 @@
-import Footer from '../components/Footer/footer';
 import Header from '../components/Header/header';
-import PostFormPopUp from '../components/PostFormPopUp/PostFormPopUp';
-import PostList from '../components/PostList';
+import PopUp from '../components/PostFormPopUp/PopUp';
+import Fab from '@mui/material/Fab';
+import PersonIcon from '@mui/icons-material/Person';
+import { useHistory } from 'react-router-dom';
 
 const HomePage = () => {
+  const history = useHistory();
+  const moveRoot = () => {
+    history.push('/');
+  };
+
   return (
     <div className="d-flex flex-column vh-100">
       <Header />
       <div className="flex-grow-1 d-flex justify-content-center align-items-center">
         <main>
           {/* ここに主要なコンテンツを追加 */}
-          <PostList />
-          {/* <PostFormPopUp /> */}
+          {/* <PostList /> */}
+          <PopUp />
+          <div>
+            <Fab
+              onClick={moveRoot}
+              color="secondary"
+              aria-label="add"
+              sx={{
+                position: 'absolute',
+                bottom: (theme: any) => theme.spacing(2),
+                left: (theme: any) => theme.spacing(2),
+              }}
+            >
+              <PersonIcon />
+            </Fab>
+          </div>
         </main>
       </div>
-      <Footer />
     </div>
   );
 };
