61df82e9bbac4dbf1b85fd0454352107503c8789
asahi1199 <asahi.yoshida.1130@gmail.com>
2023-08-23 14:54:05 +0900

    投稿一覧 (フロント機能) 完成

--- frontend/app/src/components/PostList/index.tsx
+++ frontend/app/src/components/PostList/index.tsx
@@ -1,23 +1,58 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { API_BASE_URL } from '../../containers/API_BASE_URL';
+
+interface Post {
+  id: number;
+  title: string;
+  user_name: string;
+  // num_of_comments: number;
+  // last_updated_at: string;
+}
+
 const PostList: React.FC = () => {
-  const posts = [
-    { id: 1, title: 'Post 1', username: 'User1' },
-    { id: 2, title: 'Post 2', username: 'User2' },
-  ];
+  // const posts = [
+  //   { id: 1, title: 'Post 1', username: 'User1' },
+  //   { id: 2, title: 'Post 2', username: 'User2' },
+  // ];
+
+  const [postData, setPostData] = useState<Post[] | null>(null);
+
+  useEffect(() => {
+    const fetchPostData = async () => {
+      try {
+        const fetchedPostData: Post[] | null = (
+          await axios.get(`${API_BASE_URL}/post/`)
+        ).data;
+        return setPostData(fetchedPostData);
+      } catch (error) {
+        console.log('Error fetching data:', error);
+      }
+    };
+
+    fetchPostData();
+  }, []);
+
+  console.log(postData);
 
   return (
     <div>
       <h2>投稿一覧</h2>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id} className="list-unstyled">
-            <div>
-              <p>{post.username}</p>
-              <h3>{post.title}</h3>
-              <div></div>
-            </div>
-          </li>
-        ))}
-      </ul>
+      {postData ? (
+        <ul>
+          {postData.map((post) => (
+            <li key={post.id} className="list-unstyled">
+              <div>
+                {/* <p>{post.user_name}</p> */}
+                <h3>{post.title}</h3>
+                <div></div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>データを読み込んでいます...</p>
+      )}
     </div>
   );
 };
