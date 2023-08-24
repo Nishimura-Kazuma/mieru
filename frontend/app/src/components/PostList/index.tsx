9b44826283df72c5530d10ca0b0daf6f8c5e49b1
asahi1199 <asahi.yoshida.1130@gmail.com>
2023-08-24 10:56:36 +0900

    投稿一覧 スコープによる分類機能完成

--- frontend/app/src/components/PostList/index.tsx
+++ frontend/app/src/components/PostList/index.tsx
@@ -1,8 +1,13 @@
+import * as React from 'react';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { API_BASE_URL } from '../../containers/API_BASE_URL';
 import PostCell from '../../components/PostCell';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 interface Post {
   id: number;
@@ -18,6 +23,39 @@ interface PostListByScope {
   scopeAll: Post[];
   scopeOnlyTeachers: Post[];
 }
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+const CustomTabPanel = (props: TabPanelProps) => {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+};
+
+const a11yProps = (index: number) => {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+};
 
 const PostList: React.FC = () => {
   // const posts = [
@@ -60,27 +98,60 @@ const PostList: React.FC = () => {
   const loginUserData = useSelector((state: any) => state.userData);
   // console.log(loginUserData.userData);
 
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
   return (
     <div>
       <h2 className="Heading">相談一覧</h2>
       {postData && loginUserData ? (
         <>
           {loginUserData.userData.user_attribute === '保育士' ? (
-            // ボタンの設置 + スコープによるメッセージの分類
-            <div className="CardList">
-              {postData.scopeOnlyTeachers.map((post) => (
-                <div key={post.id}>
-                  <PostCell
-                    title={post.title}
-                    userName={post.user_name}
-                    isCompleted={post.is_completed}
-                  />
+            <Box sx={{ width: '100%' }}>
+              <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <Tabs
+                  value={value}
+                  onChange={handleChange}
+                  aria-label="basic tabs example"
+                >
+                  <Tab label="保育士向け" {...a11yProps(0)} />
+                  <Tab label="全体向け" {...a11yProps(1)} />
+                </Tabs>
+              </Box>
+              <CustomTabPanel value={value} index={0}>
+                {/* 保護者のみ向けの相談 */}
+                <div className="CardList">
+                  {postData.scopeOnlyTeachers.map((post) => (
+                    <div key={post.id}>
+                      <PostCell
+                        title={post.title}
+                        userName={post.user_name}
+                        isCompleted={post.is_completed}
+                      />
+                    </div>
+                  ))}
                 </div>
-              ))}
-            </div>
+              </CustomTabPanel>
+              <CustomTabPanel value={value} index={1}>
+                {/* 全体向けの相談 */}
+                <div className="CardList">
+                  {postData.scopeAll.map((post) => (
+                    <div key={post.id}>
+                      <PostCell
+                        title={post.title}
+                        userName={post.user_name}
+                        isCompleted={post.is_completed}
+                      />
+                    </div>
+                  ))}
+                </div>
+              </CustomTabPanel>
+            </Box>
           ) : (
             <>
-              <p>保護者: スコープが all の質問のみを表示</p>
               <div className="CardList">
                 {postData.scopeAll.map((post) => (
                   <div key={post.id}>
