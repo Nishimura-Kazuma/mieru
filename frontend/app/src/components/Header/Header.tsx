ed6a8b4467a52d3a21d2203c9f3d9f35fab461be
Suzumura Yuki <s2110357@gl.cc.uec.ac.jp>
2023-08-25 11:08:01 +0900

    ヘッダーの大きさとその他表示を解消

--- frontend/app/src/components/Header/Header.tsx
+++ frontend/app/src/components/Header/Header.tsx
@@ -1,6 +1,9 @@
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import MIERUN_LOGO from '../../assets/mierun_logo.png';
+import './img.css';
+
+const points = [200, 130, 70, 250, 20, 80, 550, 54];
 
 const Header = () => {
   const history = useHistory();
@@ -15,13 +18,17 @@ const Header = () => {
     <>
       <header className="fixed-top d-flex justify-content-between align-items-center p-3 bg-light w-100 border-bottom">
         <div onClick={moveHome}>
-          <img src={MIERUN_LOGO} alt="Logo" />
+          <img src={MIERUN_LOGO} alt="Logo" className="imgwidth" />
         </div>
         <div style={{ fontSize: '20px' }}>
-          name:{' '}
-          {userData && userData.userData.name
-            ? userData.userData.name
-            : 'Loading...'}
+          {userData && userData.userData.name ? (
+            <>
+              <div>{userData.userData.name}</div>
+              <div>{points[userData.userData.id]}</div>
+            </>
+          ) : (
+            'Loading...'
+          )}
         </div>
       </header>
       <div className="headerup"></div>
